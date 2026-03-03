167036c3b0b42f3fa82c151338d9d247a5e30ade
Gaspouh <gaspard.grenier22@gmail.com>
2026-03-03 20:29:58 +0100

    création joeur et mouvements basiques + petite zone de test

--- perso.py
+++ perso.py
@@ -1,2 +1,118 @@
-a
+import pygame 
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+window.fill((200, 200, 200))
+
+# PARAMETRES MONDE
+GRAVITY = 0.4
+FRICTION = -0.15
+ACCELERATION = 0.7
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        # STATS DU JOUEUR
+        self.health = 5
+        self.max_health = 5
+        self.attack = 8
+
+        self.position = pygame.math.Vector2(x, y)
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, 0)
+
+        self.jump_strength = -10
+        self.on_ground = False
+        self.is_jumping = True
+
+        # HITBOX
+        self.rect = pygame.Rect(x, y, 60, 90)
+        original_image = pygame.image.load('player.png').convert_alpha()
+        self.image = pygame.transform.scale(original_image, (75, 90))
+
+    def update(self, platforms):
+        self.acceleration = pygame.math.Vector2(0, GRAVITY)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]and not keys[pygame.K_RIGHT]:
+            self.acceleration.x = -ACCELERATION
+        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            self.acceleration.x = ACCELERATION
+
+        self.acceleration.x += self.velocity.x * FRICTION
+        self.velocity += self.acceleration
+
+        if self.velocity.y > 10:
+            self.velocity.y = 10
+
+        # GESTION DES HITBOXS
+
+        self.position.x += self.velocity.x + 0.5 * self.acceleration.x
+        self.rect.centerx = self.position.x
+
+        self.position.y += self.velocity.y + 0.5 * self.acceleration.y
+        self.rect.bottom = self.position.y
+
+        if self.velocity.y > 0:
+            for platform in platforms:
+                if self.rect.colliderect(platform.rect):
+                    if self.rect.bottom <= platform.rect.top + self.velocity.y+2:
+                        self.rect.bottom = platform.rect.top
+                        self.position.y = self.rect.bottom
+                        self.velocity.y = 0
+                        self.on_ground = True
+                        self.is_jumping = False
+
+    def jump(self):
+        if self.on_ground:
+            self.on_ground = False
+            self.is_jumping = True
+            self.velocity.y = self.jump_strength
+            self.rect.bottom = self.position.y
+
+    def stop_jump(self):
+        if self.is_jumping and self.velocity.y < 0:
+            self.velocity.y *= 0.5
+            self.is_jumping = False
+        
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill((100, 100, 100))
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+clock = pygame.time.Clock()
+player = Player(100, 100)
+
+platforms = [
+    Platform(0, 500, 800, 100),
+    Platform(200, 400, 200, 20),
+    Platform(500, 300, 200, 20)
+]
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.jump()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                player.stop_jump()
+    player.update(platforms)
+
+    window.fill((200, 200, 200))
+    for platform in platforms:
+        window.blit(platform.image, platform.rect)
+    
+    image_rect = player.image.get_rect(midbottom=player.rect.midbottom)
+    window.blit(player.image, image_rect)
+    
+    pygame.display.flip()
+    clock.tick(60)
+    
 
