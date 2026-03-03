f5997d0ee05d77633793934dbf1b1f9b46c0ccbf
Vadim <bfbmn5gwtk@privaterelay.appleid.com>
2026-03-03 17:05:19 +0100

    bug

--- ennemi.py
+++ ennemi.py
@@ -8,19 +8,18 @@ class ennemi_debutant:
         self.ecran = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
         # Charger une image d'ennemi
-        self.sheet = pygame.image.load('insecte_sheet 2.png').convert_alpha()
+        self.sheet = pygame.image.load('insecte_sheet2.png').convert_alpha()
         self.frames_droite = []
         self.frames_gauche = []
         for i in range(8): 
-            frame = self.changer_frame(i, 55, 55)
+            frame = self.changer_frame(i, 70, 50)
             self.frames_droite.append(frame)
             self.frames_gauche.append(pygame.transform.flip(frame, True, False))
 
         self.index_image = 0.0
         self.vitesse_animation = 0.1
         self.direction = 1  # 1 pour droite, -1 pour gauche
-        self.vitesse_deplacement = 1.5
-
+        self.vitesse_deplacement = 1.70
         self.position_initiale_x = 100
         self.distance_patrouille = 400 # Distance max avant de faire demi-tour
 
@@ -47,7 +46,7 @@ class ennemi_debutant:
     def changer_frame(self, index, width, height):
         # Extraire une frame de la feuille de sprite
         frame = pygame.Surface((width, height), pygame.SRCALPHA)
-        frame.blit(self.sheet, (0, 0), (index * width,250 , width, height))
+        frame.blit(self.sheet, (0, 0), (13+index * width,250 , width, height))
         return frame
     
     def gestion_animation(self):
