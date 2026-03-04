dde385051f57e8d1f072cd3f4d63a089e752b30d
Vadim <bfbmn5gwtk@privaterelay.appleid.com>
2026-03-04 15:28:20 +0100

    classe universelle ennemi

--- ennemi.py
+++ ennemi.py
@@ -2,36 +2,33 @@ import pygame
 
 pygame.init()
 
-class ennemi_debutant():
-    def __init__(self, fenetre):
+class ennemi_debutant(pygame.sprite.Sprite):
+    def __init__(self, fenetre, x, y, sprite_sheet, nb_frames, width, height, vitesse, marge):
+        super().__init__()
         self.ecran = fenetre
         self.clock = pygame.time.Clock()
         # Charger une image d'ennemi
-        self.sheet = pygame.image.load('insecte_sheet2.png').convert_alpha()
+        self.sheet = pygame.image.load(sprite_sheet).convert_alpha()
         self.frames_droite = []
         self.frames_gauche = []
-        for i in range(8): 
+        for i in range(nb_frames): 
             # stocker les frames d'animation dans des listes pour la droite et la gauche
-            frame = self.changer_frame(i, 70, 50)
+            frame = self.changer_frame(i, width, height, marge)
             self.frames_droite.append(frame)
             self.frames_gauche.append(pygame.transform.flip(frame, True, False))
         
-        # variable d'aniamtion
+        # variable d'animation
         self.index_image = 0.0
         self.vitesse_animation = 0.1
-        self.direction = 1  # 1 pour droite, -1 pour gauche
-        self.vitesse_deplacement = 1.70
-        self.position_initiale_x = 100 # Position de départ de l'ennemi sur l'axe x
-        self.distance_patrouille = 400 # Distance max avant de faire demi-tour
 
         # définir coordonnées de l'ennemi
         self.image = self.frames_droite[0]
-        self.rect = self.image.get_rect(center=(self.position_initiale_x, 300))
-    
-    def changer_frame(self, index, width, height):
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def changer_frame(self, index, width, height, marge):
         # Extraire une frame de la feuille de sprite
         frame = pygame.Surface((width, height), pygame.SRCALPHA)
-        frame.blit(self.sheet, (0, 0), (13+index * width,250 , width, height))
+        frame.blit(self.sheet, (0, 0), (marge + (index * width), 250 , width, height))
         return frame
     
     def gestion_animation(self):
@@ -40,6 +37,20 @@ class ennemi_debutant():
         if self.index_image >= len(self.frames_droite):
             # Réinitialiser l'index pour recommencer l'animation
             self.index_image = 0.0
+
+class Araignee(ennemi_debutant):
+    def __init__(self, fenetre, x, y):
+        # On applique les caractéristique de l'ennemi débutant à l'araignée
+        super().__init__(fenetre, x, y, 'insecte_sheet2.png', 8, 70, 50, 1.7, 13)
+
+        self.direction = 1  # 1 pour droite, -1 pour gauche
+        self.vitesse_deplacement = 1.7
+        self.position_initiale_x = x # Position de départ de l'ennemi sur l'axe x
+        self.distance_patrouille = 200 # Distance max avant de faire demi-tour
+
+    def patrouille(self):
+        ennemi_debutant.gestion_animation(self)
+        # Afficher la bonne frame en fonction de la direction (orientation perso)
         self.image = self.frames_droite[int(self.index_image)] if self.direction == 1 else self.frames_gauche[int(self.index_image)]
         self.rect.x += self.vitesse_deplacement * self.direction
         # Vérifier si l'ennemi a atteint la distance de patrouille et changer de direction si nécessaire
@@ -47,6 +58,29 @@ class ennemi_debutant():
             self.direction = -1
         elif self.rect.x <= self.position_initiale_x:
             self.direction = 1
+    
+class Volant(ennemi_debutant):
+    def __init__(self, fenetre, x, y):
+        # On applique les caractéristique de l'ennemi débutant au volant
+        super().__init__(fenetre, x, y, 'sprit_sheet_volant.png', 4, 50, 50, 1.7, 5)
+
+        self.direction = 1  # 1 pour droite, -1 pour gauche
+        self.vitesse_deplacement = 1.7
+        self.position_initiale_x = x # Position de départ de l'ennemi sur l'axe x
+        
+    def poursuite(self, player_rect):
+            ennemi_debutant.gestion_animation(self)
+            # Afficher la bonne frame en fonction de la direction (orientation perso)
+            self.image = self.frames_droite[int(self.index_image)] if self.direction == 1 else self.frames_gauche[int(self.index_image)]
+            if player_rect.x > self.rect.x:
+                self.direction = 1 # Aller vers la droite
+            else:
+                self.direction = -1 # Aller vers la gauche
+            self.rect.x += self.vitesse_deplacement * self.direction # Déplacer horizontalement vers le joueur
+            if player_rect.y > self.rect.y: # Si le joueur est en dessous de l'ennemi, descendre
+                self.rect.y += self.vitesse_deplacement
+            else:
+                self.rect.y -= self.vitesse_deplacement # Si le joueur est au dessus de l'ennemi, monter
 
 if __name__ == "__main__":
      # Créer une fenêtre de jeu
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 from perso import Player, Platform
-from ennemi import ennemi_debutant
+from ennemi import ennemi_debutant, Araignee, Volant
 
 pygame.init()
+# Créer une fenêtre de jeu
 fenetre = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+# Définir le titre de la fenêtre
 pygame.display.set_caption("Shadow Legacy")
  
-araignee = ennemi_debutant(fenetre)
+# Création des objets du jeu
+araignee1 = Araignee(fenetre, 470, 280)
+volant1 = Volant(fenetre, 400, 200)
 player = Player(100, 100, fenetre)
 platforms = [
     Platform(0, 500, 800, 100),
@@ -15,30 +19,51 @@ platforms = [
     Platform(500, 300, 200, 20)
 ]
 
+# Liste des ennemis
+araignee = [araignee1]
+volant = [volant1]
+
 continuer = True
 while continuer:
     for event in pygame.event.get():
+        # quitter le jeu
         if event.type == pygame.QUIT:
             continuer = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                # faire sauter le joueur
                 player.jump()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
+                # arrêter le saut du joueur pour permettre un saut plus court
                 player.stop_jump()
-    player.update(platforms)
-    araignee.gestion_animation()
+
+    player.update(platforms)# Mettre à jour le joueur avec les plateformes pour gérer les collisions
+    
+    for elem in araignee:
+        #lancer la fonction de patrouille pour chaque araignée
+        elem.patrouille()
+    for elem in volant:
+        #lancer la fonction de poursuite pour chaque volant
+        elem.poursuite(player.rect)
+    
       # Remplir le fond 
     fenetre.fill((135, 206, 235))  # Couleur de fond (ciel bleu)
 
     for platform in platforms:
+        # Dessiner les plateformes
         fenetre.blit(platform.image, platform.rect)
 
     # Dessiner les personnages
-    fenetre.blit(araignee.image, araignee.rect)
+    for elem in araignee:
+        fenetre.blit(elem.image, elem.rect)
+    for elem in volant:
+        fenetre.blit(elem.image, elem.rect)
     image_rect = player.image.get_rect(midbottom=player.rect.midbottom)
     fenetre.blit(player.image, image_rect)
     
+# Mettre à jour l'affichage
     pygame.display.flip()
     clock.tick(60)
+
 pygame.quit()
