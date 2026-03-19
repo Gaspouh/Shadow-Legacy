1772caaf824458202581106b75d8862ce3105bec
Vadim <bfbmn5gwtk@privaterelay.appleid.com>
2026-03-19 18:08:21 +0100

    début correction bug

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -1,40 +1,45 @@
 import pygame
 
-def charger_sprite(fenetre, x, y, sprite_sheet, nb_frames, width, height, vitesse, marge):
+class animation:
+    def __init__(self, fenetre, x, y, sprite_sheet, nb_frames, width, height, marge):
+            
+        ecran = fenetre
         
-    ecran = fenetre
-    
-    # Charger une image d'ennemi
-    sheet = pygame.image.load(sprite_sheet).convert_alpha()
-    frames_droite = []
-    frames_gauche = []
-    for i in range(nb_frames): 
-        # stocker les frames d'animation dans des listes pour la droite et la gauche
-        frame = changer_frame(i, width, height, marge)
-        frames_droite.append(frame)
-        frames_gauche.append(pygame.transform.flip(frame, True, False))
-    
-    # variable d'animation
-    index_image = 0.0
-    vitesse_animation = 0.1
+        # Charger une image d'ennemi
+        sheet = pygame.image.load(sprite_sheet).convert_alpha()
+        frames_droite = []
+        frames_gauche = []
+        for i in range(nb_frames): 
+            # stocker les frames d'animation dans des listes pour la droite et la gauche
+            frame = self.changer_frame(i, width, height, marge, sheet)
+            frames_droite.append(frame)
+            frames_gauche.append(pygame.transform.flip(frame, True, False))
+        
+        # variable d'animation
+        index_image = 0.0
+        vitesse_animation = 0.1
 
-    # définir coordonnées du sprite
-    image = frames_droite[0]
-    rect = image.get_rect(center=(x, y))
+        # définir coordonnées du sprite
+        image = frames_droite[0]
+        rect = image.get_rect(center=(x, y))
 
-    # mouvement du sprite
-    velocity_x = 0
-    velocity_y = 0
+        # mouvement du sprite
+        self.frames_droite = frames_droite
+        self.frames_gauche = frames_gauche
+        self.rect = rect
+        self.index_image = index_image
+        self.vitesse_animation = vitesse_animation
 
-    def changer_frame(index, width, height, marge):
-        # Extraire une frame de la feuille de sprite
-        frame = pygame.Surface((width, height), pygame.SRCALPHA)
-        frame.blit(sheet, (0, 0), (marge + (index * width), 250 , width, height))
-        return frame
-    
-    def gestion_animation():
-        # Mettre à jour l'index de la frame pour l'animation
-        index_image += vitesse_animation
-        if index_image >= len(frames_droite):
-            # Réinitialiser l'index pour recommencer l'animation
-            index_image = 0.0
+    def changer_frame(self, index, width, height, marge, sheet ):
+            # Extraire une frame de la feuille de sprite
+            frame = pygame.Surface((width, height), pygame.SRCALPHA)
+            frame.blit(sheet, (0, 0), (marge + (index * width), 250 , width, height))
+            return frame
+        
+    def gestion_animation(self, index_image, vitesse_animation, frames_droite):
+            # Mettre à jour l'index de la frame pour l'animation
+            index_image += vitesse_animation
+            if index_image >= len(frames_droite):
+                # Réinitialiser l'index pour recommencer l'animation
+                index_image = 0.0
+            return index_image
--- traps.py
+++ traps.py
@@ -17,7 +17,7 @@ class Spike :
         }
 
 class Lava :
-    def __init__(self, x, y, lenght, height)
+    def __init__(self, x, y, lenght, height):
         super().__init__()
         self.image = pygame.Surface(lenght, height)
         self.image.fill((255, 0, 0))
@@ -30,7 +30,7 @@ class Lava :
         }
 
 class Acid :
-    def __init__(self, x, y, lenght, height)
+    def __init__(self, x, y, lenght, height):
         super().__init__()
         self.image = pygame.Surface(lenght, height)
         self.image.fill((0, 255, 0))
@@ -44,17 +44,17 @@ class Acid :
 
 class Thorns :
         def __init__(self, x, y):
-        super().__init__()
-    # Charger une image de ronce
-        self.sheet = pygame.image.load("insecte_sheet2.png").convert_alpha()
-        self.image = pygame.transform.scale(self.sheet, (40, 40))
-        self.rect = self.image.get_rect(topleft=(x,y))
+            super().__init__()
+        # Charger une image de ronce
+            self.sheet = pygame.image.load("insecte_sheet2.png").convert_alpha()
+            self.image = pygame.transform.scale(self.sheet, (40, 40))
+            self.rect = self.image.get_rect(topleft=(x,y))
 
-        self.attack_data = {
-            "damage" : 1,
-            "knockback_x" : 0,
-            "knockback_y" : 0
-        }
+            self.attack_data = {
+                "damage" : 1,
+                "knockback_x" : 0,
+                "knockback_y" : 0
+            }
 
 class Saw :
     def __init__(self, x, y, radius):
@@ -62,7 +62,7 @@ class Saw :
     # Charger une image de scie circulaire
         self.sheet = pygame.image.load("insecte_sheet2.png").convert_alpha()
         self.image = pygame.transform.scale(self.sheet, (radius*2, radius*2))
-        self.
+        
 
         self.attack_data = {
             "damage" : 1,
